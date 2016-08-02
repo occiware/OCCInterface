38c0f081c8f6f6245ee489b06a429451e0723e8c
romathonat <romain.mathonat@gmail.com>
2016-08-02 16:00:13 +0200

    switching prod heroku

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-var fs = require('fs');
 const querystring = require('querystring');
 
 ///var host = 'occinterface.herokuapp.com'
 var port = process.env.PORT || 3000;
-var proxyDefaultTarget = 'http://localhost:8080/';
+var proxyDefaultTarget = 'http://malmo.lizenn.net:8080';
 var proxyOptions = {
   path : '/proxiedOCCIServer/**',
   target: proxyDefaultTarget,
@@ -13,11 +12,11 @@ var proxyOptions = {
   changeOrigin: true
 };
 
-// additional conf of express or webpack dev server :
+// additional conf of express or webpack dev server : let the user update the proxy target
 function setup(app) {
   // update the proxied target URL :
   // test using ex. http://localhost:3000/conf?proxyTarget=http://malmo.lizenn.net:8080 then http://localhost:3000/proxiedOCCIServer/-/
-  app.all('/conf', function(req, res) { // TODO only post ?
+  app.all('/conf', function(req, res) {
     proxyOptions.target = querystring.parse(req._parsedUrl.query).proxyTarget;
     console.log('updated proxyOptions', proxyOptions);
     res.setHeader('Content-Type', 'application/javascript');
@@ -25,66 +24,46 @@ function setup(app) {
   });
 }
 
-
-console.log("env is prod =", isProduction, process.env.NODE_ENV);
 var isProduction = process.env.NODE_ENV === 'production';
+console.log('In production :', isProduction);
 
 if (isProduction) { // prod :
 
-// use manually produced prod bundle.js :
-fs.createReadStream('dist/bundle.js.prod').pipe(fs.createWriteStream('dist/bundle.js'));
+  var express = require('express');
+  var http = require('http');
+  var proxy = require('express-http-proxy');
 
-var express = require("express");
-var http = require("http");
-var httpProxy = require('http-proxy');
-var proxy = new httpProxy.createProxyServer({secure: false});
+  // Init express server
+  var app = new express();
 
-// Init express server
-var app = this.app = new express();
-app.use('/static/', express.static(__dirname + '/dist/')); // TODO webpack conf, rather '/public/'
-app.use(express.static(__dirname + '/')); // TODO webpack conf, rather '/public/'
-app.all(proxyOptions.path, function (req, res, next) {
-  if(typeof proxyOptions.rewrite === 'function') proxyOptions.rewrite(req, proxyOptions);
-  if (proxyOptions.host) {
-    req.headers.host = proxyOptions.host;
-  }
-  proxy.web(req, res, proxyOptions, function(err){
-    var msg = "cannot proxy to " + proxyOptions.target + " (" + err.message + ")";
-    this.sockWrite(this.sockets, "proxy-error", [msg]);
-    res.statusCode = 502;
-    res.end();
-  }.bind(this));
-}.bind(this));
-setup(app);
-var listeningApp = http.createServer(app);
-listeningApp.listen(port, function (err, result) {
-  if (err) {
-    return console.log(err);
-  }
+  app.use(express.static(__dirname + '/')); // TODO webpack conf, rather '/public/'
+  app.use('/proxiedOCCIServer', proxy(proxyOptions.target));
+
+  setup(app);
+  var listeningApp = http.createServer(app);
+  app.listen(port);
 
   console.log('Listening at http://localhost:' + port + '/');
-});
-
 
 } else { // dev :
 
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+  var webpack = require('webpack');
+  var WebpackDevServer = require('webpack-dev-server');
+  var config = require('./webpack.config');
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true,
-  proxy: [proxyOptions],
-  setup: setup
-}).listen(port, function (err, result) {
-  if (err) {
-    return console.log(err);
-  }
+  new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true,
+    proxy: [proxyOptions],
+    setup: setup
+  }).listen(port, function (err, result) {
+    if (err) {
+      return console.log(err);
+    }
 
-  console.log('Listening at http://localhost:' + port + '/');
-});
+    console.log('Listening at http://localhost:' + port + '/');
+  });
 
 }
 
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,19 +2,27 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'eval',
+  devtool: 'source-map',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
     './src/index'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: '/dist/'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
   ],
   module: {
     loaders: [{
