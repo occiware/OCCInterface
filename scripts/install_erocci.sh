b10fe661985d9f1301d3543823a49c6c68d1928a
romathonat <romain.mathonat@gmail.com>
2016-08-29 10:02:20 +0200

    changing default integratedVersion to false

--- scripts/install_erocci.sh
+++ scripts/install_erocci.sh
@@ -1,4 +1,8 @@
+#we remove uneeded dependencies
 sed -i '/express/d' package.json
+
+#we set integratedVersion to true
+sed -i -e 's/integratedVersion = false/integratedVersion = true/g' ./conf.js
 
 npm install --production
 npm run build
