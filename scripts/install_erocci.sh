8fbaf88dff844f73b35f6f297828ea598a40bd25
Jean Parpaillon <jean.parpaillon@free.fr>
2016-09-01 12:47:58 +0200

    Fix path to package.json and conf.js in install_erocci.sh script

--- scripts/install_erocci.sh
+++ scripts/install_erocci.sh
@@ -1,8 +1,8 @@
 #we remove uneeded dependencies
-sed -i '/express/d' package.json
+sed -i '/express/d' ../package.json
 
 #we set integratedVersion to true
-sed -i -e 's/integratedVersion = false/integratedVersion = true/g' ./conf.js
+sed -i -e 's/integratedVersion = false/integratedVersion = true/g' ../conf.js
 
 npm install --production
 npm run build
