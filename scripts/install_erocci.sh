c38c8d65f1bf7a529f25ee5700474768bc9db214
Jean Parpaillon <jean.parpaillon@free.fr>
2016-09-01 14:04:06 +0200

    Make install_erocci script more robust: can run from any location

--- scripts/install_erocci.sh
+++ scripts/install_erocci.sh
@@ -1,8 +1,15 @@
+#!/bin/sh
+
+set -e
+
+topdir=$(cd $(dirname $0)/.. && pwd)
+
 #we remove uneeded dependencies
-sed -i '/express/d' ../package.json
+sed -i '/express/d' ${topdir}/package.json
 
 #we set integratedVersion to true
-sed -i -e 's/integratedVersion = false/integratedVersion = true/g' ../conf.js
+sed -i -e 's/integratedVersion = false/integratedVersion = true/g' ${topdir}/conf.js
 
-npm install --production
-npm run build
+cd ${topdir} && \
+    npm install --production && \
+    npm run build
