2748015bbb27a4050ac75818439d790a2304a470
xia0ben <benoit.renault@smile.fr>
2017-07-07 15:56:41 +0200

    Move Dockerfiles and accompanying scripts to occiware-ozwillo repository

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Install necessary packages
-apt-get update && apt-get install -y build-essential libssl-dev curl
-npm install
--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Workaround for Docker Studio since it doesn't support the --add-host parameter yet :
-# Add martserver-linkeddata container IP to /etc/hosts file
-echo 172.17.0.7 martserver-linkeddata-1 >> /etc/hosts
-
-# Start the MartServer
-npm run dev
